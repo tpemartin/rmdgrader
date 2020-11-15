ee5abd4019980454616448448a5777b89f721266
tpemartin <tpemartin@gmail.com>
2020-11-15 20:05:19 +0800

    add return module

--- R/return.R
+++ R/return.R
@@ -1,3 +1,72 @@
+#' Generate synthesizer that sythersizes both correct answer and student answer
+#'
+#' @description the synthesizer takes in only one input that is the codeChunkTable produced by rmd2drake::get_listCodeChunksFromRmdlines
+#' @param correctAnsFilename A character of filepath to the correct answer Rmd
+#'
+#' @return
+#' @export
+#'
+#' @examples none
+synthersizeWithCorrectAnsFunctional <- function(correctAnsFilename){
+  require(dplyr)
+  correctAnsFilename %>%
+    xfun::read_utf8() %>%
+    rmdgrader::reviseRmd_atAns()-> ansRmdlines_revised
+
+  ansRmdlines_revised %>%
+    rmd2drake:::get_chunksTable() %>%
+    filter(
+      stringr::str_detect(object, "ans")
+    ) %>%
+    arrange(
+      begin
+    )  -> ansChunkTable
+
+  augment_studentRmdsWithAtAnsFunctional(ansRmdlines_revised, ansChunkTable)
+}
+
+#' Generate the funtion that can combine correct answer and student answer into one Rmd
+#'
+#' @param ansRmdAccommodateAtAns A character of lines from ansRmd that already accommodate \%at\% ans
+#' @param ansChunkTable A data frame that show code chunk mapping of ansRmdAccommodateAtAns
+#'
+#' @return
+#' @export
+#'
+#' @examples none
+augment_studentRmdsWithAtAnsFunctional <-
+  function(ansRmdAccommodateAtAns, ansChunkTable) {
+    ansChunkTable$object -> ansLabels
+    ansChunkTable$begin -> ansBegins
+
+    function(studentCodeChunks) {
+      ansRmd_augmentStudent <- ansRmdAccommodateAtAns
+
+      # .x=1
+      for (.x in seq_along(ansLabels)) {
+        cutLoc <- ansBegins[[.x]] - 2
+        ansLabelNow <- ansLabels[[.x]]
+        nLines <- length(studentCodeChunks[[ansLabelNow]])
+        # ansRmd_augmentStudent[1:(ansBegins[[.x]])]
+
+        ansRmd_augmentStudent <-
+          c(
+            ansRmd_augmentStudent[1:cutLoc],
+            "",
+            "```{r}",
+            studentCodeChunks[[ansLabelNow]],
+            "```",
+            "",
+            ansRmd_augmentStudent[-c(1:cutLoc)]
+          )
+        ansBegins <-
+          ansBegins + nLines + 4
+      }
+      return(ansRmd_augmentStudent)
+    }
+  }
+
+
 #' Revise Rmd to accommodate \%at\% ans
 #'
 #' @param ansRmdlines A character of Rmd lines
@@ -9,7 +78,6 @@
 reviseRmd_atAns <- function(ansRmdlines)
 {
   require(dplyr)
-  ansRmdlines <- xfun::read_utf8(params$correctAnsFilepath)
   # insert one empty line to assure there is line after the last code chunk.
   ansRmdlines <- c(ansRmdlines,"")
   chunkTable <- ansRmdlines %>% rmd2drake::get_chunksTable(exclude = "(afterMake=T|drake=F)")
